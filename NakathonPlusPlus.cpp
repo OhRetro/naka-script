7df799e146b0087c554b6d2c1a8e6b5d207398e7
OhRetro <gabrielnacazone@hotmail.com>
2024-02-22 12:19:23 -0300

    Initial commit

---
+++ NakathonPlusPlus.cpp
@@ -0,0 +1,12 @@
+﻿// NakathonPlusPlus.cpp : Defines the entry point for the application.
+//
+
+#include "NakathonPlusPlus.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
